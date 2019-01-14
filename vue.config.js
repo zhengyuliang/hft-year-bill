8bf5f3623e065dbc152325cf1ec14845cff0f3a6
bangwei.xu <bangwei.xu@esmart365.com>
2019-01-14 12:14:43 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    baseUrl: './',
+    configureWebpack: config => {
+        require('vux-loader').merge(config, {
+            options: {},
+            plugins: ['vux-ui']
+        })
+    },
+    devServer: {}
+}
