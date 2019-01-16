6d0f9ffeb44e3f463b0471130eb25e0d70579d0e
bangwei.xu <bangwei.xu@esmart365.com>
2019-01-16 09:47:24 +0800

    路由到

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import echarts from 'echarts' 
+import './global.scss'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="wrap">
-    首页
+    <div class="sales-bg"></div>
   </div>
 </template>
+<style lang="scss" scoped>
+.sales-bg {
+  background-image: url("../assets/login-bg.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 100% 100%;
+  width: 100%;
+  bottom: 0;
+  top: 0;
+  position: absolute;
+}
+</style>
